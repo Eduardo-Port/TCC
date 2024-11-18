33cdb075398cbabddecc518c62b6390606799ca2
CaiqueDS <117022913+caiquesantosss@users.noreply.github.com>
2024-11-17 21:58:14 -0300

    *

--- frontend/JS/Carrinho.js
+++ frontend/JS/Carrinho.js
@@ -2,6 +2,7 @@ const carrinhoLateral = document.getElementById("carrinho-lateral")
 const carrinhoReal = document.querySelector('.carrinho-toggler')
 
 
+// Toggling the Sidebar
 carrinhoReal.addEventListener('click', () => {
     carrinhoLateral.className = "carrinho-legal show"
 });
@@ -9,6 +10,7 @@ carrinhoReal.addEventListener('click', () => {
 console.log(localStorage.getItem('token'))
 
 
+// Closing the Sidebar on clicking Outside and on the Sidebar-Links
 window.addEventListener('click', (e) => {
     if (e.target.id !== 'carrinho-lateral' && e.target.className !== 'carrinho-toggler') {
         carrinhoLateral.classList.remove('show')
